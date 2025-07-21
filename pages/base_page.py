9c36b996c3e113ea020dd8270b471189d6214003
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-07-21 21:15:21 +0530

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,9 @@
-from playwright.sync_api import Page
-
-class BasePage:
-    def __init__(self,page: Page):
+from playwright.sync_api import Page    # To import the sync.api
+                                        # BasePage class containing common page methods
+class BasePage:                      
+    def __init__(self,page: Page):      # Constructor and page instance variable is defined
         self.page=page
-
+                                        # Method to navigate to a given URL
     def navigate(self,url):
         self.page.goto(url)
 
