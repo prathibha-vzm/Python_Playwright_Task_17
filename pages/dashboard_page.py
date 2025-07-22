45bc8ea30bfee4294264fe15f17b6cbff0c81fde
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-07-22 09:31:22 +0530

    dashboard_page.py

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,16 +1,16 @@
-from zenportal.pages.base_page import BasePage # Base class with common page methods
+from zenportal.pages.base_page import BasePage                                    # Base class with common page methods
 
-# LogOut class inheriting from BasePage
+                                                                                  # LogOut class inheriting from BasePage
 class LogOut(BasePage):
     def __init__(self, page):
-        super().__init__(page)         # Initialize the BasePage with the current page instance
-        # Defining the elements with its locators
-        self.profile_icon=page.locator(".profile-click-icon-div")
-        self.logout_text=page.get_by_text("Log out")
+        super().__init__(page)                                                    # Initialize the BasePage with the current page instance
+           self.profile_icon=page.locator(".profile-click-icon-div")              # Defining the elements with its locators
+           self.logout_text=page.get_by_text("Log out")
+        
 
     def logout_functionality(self):
         self.profile_icon.click()                                                 # To click on profile icon
         self.logout_text.click()                                                  # To click on logout text
-        self.page.wait_for_url("https://v2.zenclass.in/login", timeout=5000)  # To wait for the logot process
+        self.page.wait_for_url("https://v2.zenclass.in/login", timeout=5000)      # To wait for the logot process
         self.page.screenshot(                                                     # To capture the logout page
             path=r"/zenportal/reports/screenshots/assert_successful_logout.png")
